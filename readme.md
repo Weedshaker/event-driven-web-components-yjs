bc3970dc1fdfdcbff962b83959220c9589708152
Weedshaker <weedshaker@gmail.com>
2022-12-09 10:09:47 +0100

    initial

---
+++ readme.md
@@ -0,0 +1,23 @@
+# Event Driven Web Components yjs
+
+> yjs web components wrapper
+
+
+### Installation
+
+- no stack!
+
+
+### TODO
+
+Everything, just started
+
+
+## Implementation
+
+Frontend Event Driven Architecture works basically like the DOM itself...
+
+
+## Credit
+
+Created by [Edmgb](https://github.com/Edmgb) + [スィルヴァン aka. Weedshaker](https://github.com/Weedshaker)
---
+++ src/Yjs.js
@@ -0,0 +1,49 @@
+// @ts-check
+
+/* global HTMLElement */
+/* global document */
+/* global self */
+/* global fetch */
+/* global CustomEvent */
+
+/**
+ * Yjs is a helper with a few functions for every web component which possibly allows a YjsRoot (atom, organism and molecule)
+ *
+ * @export
+ * @function Yjs
+ * @param {CustomElementConstructor} [ChosenHTMLElement = HTMLElement]
+ * @property {
+    connectedCallback,
+    disconnectedCallback,
+  }
+ * @return {CustomElementConstructor | *}
+ */
+export const Yjs = (ChosenHTMLElement = HTMLElement) => class Yjs extends ChosenHTMLElement {
+  /**
+   * Creates an instance of Yjs. The constructor will be called for every custom element using this class when initially created.
+   *
+   * @param {*} args
+   */
+  constructor (...args) {
+    // @ts-ignore
+    super(...args)
+
+    
+  }
+
+  /**
+   * Lifecycle callback, triggered when node is attached to the dom
+   * must be here as a placeholder
+   *
+   * @return {void}
+   */
+  connectedCallback () {}
+
+  /**
+   * Lifecycle callback, triggered when node is detached from the dom
+   * must be here as a placeholder
+   *
+   * @return {void}
+   */
+  disconnectedCallback () {}
+}
