6fe9a30e100d8a05d05376afede82715817f3952
Weedshaker <weedshaker@gmail.com>
2023-09-30 19:54:44 +0200

    test

--- MasterServiceWorker.js
+++ MasterServiceWorker.js
@@ -35,9 +35,7 @@ class MasterServiceWorker {
             client.focus()
             client.postMessage('Push notification clicked!')
           } else {
-            console.log('heeere', event.notification.data.hostAndPort
-            ? `${this.location.origin}${this.location.pathname}?websocket-url=${event.notification.data.hostAndPort}&room=${event.notification.data.room}`
-            : this.location.href);
+            console.log('heeere', this.location);
             clients.openWindow(event.notification.data.hostAndPort
               ? `${this.location.origin}${this.location.pathname}?websocket-url=${event.notification.data.hostAndPort}&room=${event.notification.data.room}`
               : this.location.href
