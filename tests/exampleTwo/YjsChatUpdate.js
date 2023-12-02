5923dbf314104e21ecbc0aaee4486a484b0adab3
Gabriel Mettler <gabriel.mettler@mgb.ch>
2023-12-02 13:19:46 +0100

    added an option container todo option functions

--- tests/exampleTwo/YjsChatUpdate.js
+++ tests/exampleTwo/YjsChatUpdate.js
@@ -10,7 +10,7 @@ export default class YjsChatUpdate extends HTMLElement {
       <style>
         :host > ul {
           margin: 0;
-          padding: 0;
+          padding: 0;          
         }
         :host > ul > li {
           background-color: lightgray;
@@ -19,7 +19,7 @@ export default class YjsChatUpdate extends HTMLElement {
           list-style: none;
           padding: 1em;
           margin: 0.25em 0.1em 0.25em 0;
-          width: 80%;
+          width: 80%;          
         }
         :host > ul > li.self {
           background-color: lightgreen;
@@ -41,6 +41,8 @@ export default class YjsChatUpdate extends HTMLElement {
         :host > ul > li > .timestamp {
           font-size: 0.6em;
         }
+
+        
       </style>
     `
     this.shadowRoot.appendChild(ul)
@@ -50,14 +52,103 @@ export default class YjsChatUpdate extends HTMLElement {
       const isScrolledBottom = this.scrollHeight < this.scrollTop + this.offsetHeight + 200 /* tollerance */
       let lastEntryIsSelf = false
       let lastMessage = null
+
+
+
       ul.innerHTML = ''
       event.detail.chat.sort((a, b) => a.timestamp - b.timestamp).forEach((entry, i, chat) => {
         const li = document.createElement('li')
+       
         if (entry.isSelf) li.classList.add('self')
         // make aTags with href when first link is detected https://stackoverflow.com/questions/1500260/detect-urls-in-text-with-javascript
         entry.text = entry.text.replace(/(https?:\/\/[^\s]+)/g, url => `<a href="${url}" target="_blank">${url}</a>`)
         li.innerHTML = `<span class="user">${entry.nickname}: </span><br><span class="text">${entry.text}</span><br><span class="timestamp">${(new Date(entry.timestamp)).toLocaleString(navigator.language)}</span>`
+        
+
+/**
+ * TODO: Create own atom components for toggle button and the optionsContainer
+ */
+
+        // Create a button for li element
+        const button = document.createElement('button');
+        button.classList.add('hover-button');
+        button.innerHTML = '▼'; // Arrow down or any other content
+        button.style.display = 'none'; // Initially hide the button
+        button.style.float = 'right';
+        button.style.borderRadius = '50%';
+        button.style.boxShadow = 'var(--box-shadow-default)';
+        button.style.border = 'none';
+        
+        const container = document.createElement('div');
+    container.classList.add('smaller-list-container');
+    container.style.display = 'none';
+    container.style.position = 'absolute'; 
+    container.style.backgroundColor = 'var(--background-color-smallListContainer)';
+    container.style.padding ='0.5rem 1rem';
+    container.style.boxShadow = 'var(--box-shadow-default)';
+    container.style.borderRadius = '10px';
+
+// Create a nested ul for the smaller list
+const smallerList = document.createElement('ul');
+smallerList.style.margin = 'auto';
+smallerList.style.padding = '0';
+
+
+const smallerListLi1 = document.createElement('li');
+smallerListLi1.innerHTML =`<span>Change Colors</span>`;
+smallerList.appendChild(smallerListLi1)
+
+
+const smallerListLi2 = document.createElement('li');
+smallerListLi2.innerHTML =`<span>Response to this message</span>`;
+smallerList.appendChild(smallerListLi2)
+
+
+
+         // Add hover event listener to display the button
+          li.addEventListener('mouseenter', () => {  
+            button.style.display = 'inline-block'; // Show the button for this li          
+            handleLiElementHover(entry);
+          });
+
+          // Add mouseleave event listener to hide the button
+    li.addEventListener('mouseleave', () => {
+
+      if (!li.isContainerVisible) {
+      button.style.display = 'none'; // Hide the button for this li
+      }
+
+    });
+
+    li.isContainerVisible = false;
+
+
+
+            // click event listener to the button
+        button.addEventListener('click', () => {
+
+          li.isContainerVisible = !li.isContainerVisible; 
+          // Get the position of the li element
+  const liRect = li.getBoundingClientRect();
+
+  // Set the position of the container next to the li element
+  container.style.display = li.isContainerVisible ? 'block' : 'none';
+    container.style.top = `${liRect.top + 50}px`;
+    container.style.left = `${liRect.right - 300}px`;
+
+  // Change button content to 'X'
+  button.innerHTML = li.isContainerVisible ? '✕' : '▼';
+  handleButtonClick(entry);
+        });
+
+        
+        li.appendChild(button);
+        
+        
         ul.appendChild(li)
+
+        container.appendChild(smallerList);
+        document.body.appendChild(container);
         if (chat.length === i + 1 && entry.isSelf) lastEntryIsSelf = true
         if (chat.length === i + 1) lastMessage = entry
       })
@@ -97,7 +188,25 @@ export default class YjsChatUpdate extends HTMLElement {
           composed: true
         }))
       }
+      
+
+     
+   
+//Event Listener Closing
     }
+
+     // function to handle the button click
+  const handleButtonClick = (entry) => {
+   
+    console.log('Button clicked for entry:', entry);
+  };
+
+  // function to handle the LI element hover
+  const handleLiElementHover = (entry) => {
+    console.log('Li Element is hovered for showing btn:', entry);
+  };
+
+
   }
 
   connectedCallback () {
