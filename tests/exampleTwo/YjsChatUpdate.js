eb64141afeb539825522dc919a5f278e79757b1c
Weedshaker <weedshaker@gmail.com>
2023-12-11 14:53:46 +0100

    linter and fix

--- tests/exampleTwo/YjsChatUpdate.js
+++ tests/exampleTwo/YjsChatUpdate.js
@@ -85,100 +85,85 @@ export default class YjsChatUpdate extends HTMLElement {
       let lastEntryIsSelf = false
       let lastMessage = null
 
-
-
       ul.innerHTML = ''
       event.detail.chat.sort((a, b) => a.timestamp - b.timestamp).forEach((entry, i, chat) => {
         const li = document.createElement('li')
-       
+
         if (entry.isSelf) li.classList.add('self')
         // make aTags with href when first link is detected https://stackoverflow.com/questions/1500260/detect-urls-in-text-with-javascript
         entry.text = entry.text.replace(/(https?:\/\/[^\s]+)/g, url => `<a href="${url}" target="_blank">${url}</a>`)
         li.innerHTML = `<span class="user">${entry.nickname}: </span><br><span class="text">${entry.text}</span><br><span class="timestamp">${(new Date(entry.timestamp)).toLocaleString(navigator.language)}</span>`
-        
 
-/**
+        /**
  * TODO: Create own atom components for toggle button and the optionsContainer
  */
 
         // Create a button for li element
-        const button = document.createElement('button');
-        button.classList.add('hover-button');
-        button.innerHTML = '▼'; // Arrow down or any other content
-        button.style.display = 'none'; // Initially hide the button
-        
-        
-        const container = document.createElement('div');
-    container.classList.add('smaller-list-container');
+        const button = document.createElement('button')
+        button.classList.add('hover-button')
+        button.innerHTML = '▼' // Arrow down or any other content
+        button.style.display = 'none' // Initially hide the button
 
+        const container = document.createElement('div')
+        container.classList.add('smaller-list-container')
 
-// Create a nested ul for the smaller list
-const smallerList = document.createElement('ul');
-smallerList.style.margin = 'auto';
-smallerList.style.padding = '0';
+        // Create a nested ul for the smaller list
+        const smallerList = document.createElement('ul')
+        smallerList.style.margin = 'auto'
+        smallerList.style.padding = '0'
 
-const smallerListLi1 = document.createElement('li');
-smallerListLi1.innerHTML =`<span><span>🗑</span> Delete Message</span>`;
-smallerList.appendChild(smallerListLi1)
+        const smallerListLi1 = document.createElement('li')
+        smallerListLi1.innerHTML = '<span><span>🗑</span> Delete Message</span>'
+        smallerList.appendChild(smallerListLi1)
 
-/* TODO: Add functionality to change color for user in room
+        /* TODO: Add functionality to change color for user in room
 const smallerListLi1 = document.createElement('li');
 smallerListLi1.innerHTML =`<span>Change Colors</span>`;
-smallerList.appendChild(smallerListLi1)*/
+smallerList.appendChild(smallerListLi1) */
 
-/* TODO: Add functionality to Response to Message
+        /* TODO: Add functionality to Response to Message
 const smallerListLi2 = document.createElement('li');
 smallerListLi2.innerHTML =`<span>Response to this message</span>`;
-smallerList.appendChild(smallerListLi2)*/
+smallerList.appendChild(smallerListLi2) */
 
+        // Add hover event listener to display the button
+        li.addEventListener('mouseenter', () => {
+          button.style.display = 'inline-block' // Show the button for this li
+          handleLiElementHover(entry)
+        })
 
+        // Add mouseleave event listener to hide the button
+        li.addEventListener('mouseleave', () => {
+          if (!li.isContainerVisible) {
+            button.style.display = 'none' // Hide the button for this li
+          }
+        })
 
+        li.isContainerVisible = false
 
-
-         // Add hover event listener to display the button
-          li.addEventListener('mouseenter', () => {  
-            button.style.display = 'inline-block'; // Show the button for this li          
-            handleLiElementHover(entry);
-          });
-
-          // Add mouseleave event listener to hide the button
-    li.addEventListener('mouseleave', () => {
-
-      if (!li.isContainerVisible) {
-      button.style.display = 'none'; // Hide the button for this li
-      }
-
-    });
-
-    li.isContainerVisible = false;
-
-
-
-            // click event listener to the button
+        // click event listener to the button
         button.addEventListener('click', () => {
-
-          li.isContainerVisible = !li.isContainerVisible; 
+          li.isContainerVisible = !li.isContainerVisible
           // Get the position of the li element
-  //const liRect = li.getBoundingClientRect();
+          // const liRect = li.getBoundingClientRect();
 
-  // Set the position of the container next to the li element
-  container.style.display = li.isContainerVisible ? 'block' : 'none';
-  /*  container.style.top = `${liRect.top + 50}px`;
+          // Set the position of the container next to the li element
+          container.style.display = li.isContainerVisible ? 'block' : 'none'
+          /*  container.style.top = `${liRect.top + 50}px`;
     container.style.left = `${liRect.right - 300}px`;
 */
-  // Change button content to 'X'
-  button.innerHTML = li.isContainerVisible ? '✕' : '▼';
-  handleButtonClick(entry);
-        });
+          // Change button content to 'X'
+          button.innerHTML = li.isContainerVisible ? '✕' : '▼'
+          handleButtonClick(entry)
+        })
 
-        
-        li.appendChild(button);
-        
-        
+        li.appendChild(button)
+
         ul.appendChild(li)
 
-        container.appendChild(smallerList);
-        li.appendChild(container);
+        container.appendChild(smallerList)
+        // TODO: fix up this file (redo at chat repo properly) and then add this feature
+        // li.appendChild(container)
         if (chat.length === i + 1 && entry.isSelf) lastEntryIsSelf = true
         if (chat.length === i + 1) lastMessage = entry
       })
@@ -218,26 +203,19 @@ smallerList.appendChild(smallerListLi2)*/
           composed: true
         }))
       }
-      
 
-
-     
-   
-//Event Listener Closing
+      // Event Listener Closing
     }
 
-     // function to handle the button click
-  const handleButtonClick = (entry) => {
-   
-    console.log('Button clicked for entry:', entry);
-  };
+    // function to handle the button click
+    const handleButtonClick = (entry) => {
+      console.log('Button clicked for entry:', entry)
+    }
 
-  // function to handle the LI element hover
-  const handleLiElementHover = (entry) => {
-    console.log('Li Element is hovered for showing btn:', entry);
-  };
-
-
+    // function to handle the LI element hover
+    const handleLiElementHover = (entry) => {
+      console.log('Li Element is hovered for showing btn:', entry)
+    }
   }
 
   connectedCallback () {
